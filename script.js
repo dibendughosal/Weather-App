51e3384b1cfdc3fcf7270e76a5682288cd3a65f4
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-01-23 21:54:45 +0530

    API Call Done

--- script.js
+++ script.js
@@ -17,6 +17,17 @@ function switchTab(clickedTab){
         currentTab.classList.remove('current-tab');
         currentTab = clickedTab;
         currentTab.classList.add('current-tab'); 
+
+        if(!searchForm.classList.contains('active')){
+            userInfoContainer.classList.remove('active');
+            grantLocationCotainer.classList.remove('active');
+            searchForm.classList.add('active');
+        }
+        else{
+            searchForm.classList.remove('active');
+            userInfoContainer.classList.remove('active');
+            getfromSessionStorage();
+        }
     }
 }
 userTab.addEventListener('click',() => {
@@ -25,3 +36,48 @@ userTab.addEventListener('click',() => {
 searchTab.addEventListener('click',() => {
     switchTab(searchTab);
 });
+// check  if cordinates are already present in session storage.
+function getfromSessionStorage(){
+    const localCoordinates = sessionStorage.getItem('user-coordinates');
+    if(!localCoordinates){
+        // if local coordinates is not found
+        grantLocationCotainer.classList.add('active');
+    }
+    else{
+        const coordinates = JSON.parse(localCoordinates);
+        fetchUserWeatherInfo(coordinates);
+    }
+}
+
+async function fetchUserWeatherInfo(coordinates) {
+    const {lat, lon} = coordinates;
+    //make grant access container invisible
+    grantLocationCotainer.classList.remove('active');
+    // make loader visible
+    loadingScreen.classList.add('active');
+
+    // API Call
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
+        const data = await response.json();
+        loadingScreen.classList.remove('active');
+        userInfoContainer.classList.add('active');
+        renderWeatherInfo(data);
+    } catch (error) {
+        loadingScreen.classList.remove('active')
+    }
+}
+
+function renderWeatherInfo(weatherInfo){
+    // Fetch the Element
+    const cityName = document.querySelector('[data-cityName]');
+    const countryIcon = document.querySelector("[data-countryIcon]");
+    const weatherDesc = document.querySelector('[data-weatherDesc]');
+    const weatherIcon = document.querySelector('[data-weatherIcon]');
+    const temp = document.querySelector('[data-temp]');
+    const windspeed = document.querySelector('[data-windSpeed]');
+    const humidity = document.querySelector('[data-humidity]');
+    const cloudiness = document.querySelector('[data-cloudiness]')
+
+    
+}
